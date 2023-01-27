6040cb259caa75a772500fcc82442e13ee1a253d
angle361 <sanyam.student.mec19@itbhu.ac.in>
2023-01-28 02:08:03 +0530

    initial commit

---
+++ client/build/precache-manifest.f5e0a249f00e1279220db783bf9071f3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b714bcb09ed50f02b02f276e07218aa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a6e6e2b6980c10f2098",
+    "url": "/static/js/2.ebf9d71e.chunk.js"
+  },
+  {
+    "revision": "a20f09fdd131ee96d9198102c35895bc",
+    "url": "/static/js/2.ebf9d71e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd5a02cbd40fe372e54e",
+    "url": "/static/js/main.704749e5.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.704749e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a50bf9c8c8828f9a60",
+    "url": "/static/js/runtime-main.5303953d.js"
+  },
+  {
+    "revision": "052f0788fed203a7aeb3336908d774de",
+    "url": "/static/media/Axelerate.052f0788.jpg"
+  },
+  {
+    "revision": "0e872fcb19e0aadb732d4c6a5e4bade1",
+    "url": "/static/media/aquadrivelow.0e872fcb.jpg"
+  },
+  {
+    "revision": "8a8ad2b932abc6ad58829100563cb67a",
+    "url": "/static/media/atv-gallery.8a8ad2b9.png"
+  },
+  {
+    "revision": "7a8797e49f993f234ace155014c7cd2b",
+    "url": "/static/media/atv.7a8797e4.jpg"
+  },
+  {
+    "revision": "74331f50fb49c494905d3779a56c6010",
+    "url": "/static/media/axelerate20-1.74331f50.jpg"
+  },
+  {
+    "revision": "67b34c07485c4788ffc75d8bce89195f",
+    "url": "/static/media/design.67b34c07.jpg"
+  },
+  {
+    "revision": "8cbe39174a9d6ad905bb244250e4c591",
+    "url": "/static/media/harsh.8cbe3917.jpg"
+  },
+  {
+    "revision": "2cf54d5b14d7c1d9022ab20916bd6f37",
+    "url": "/static/media/lady-img.2cf54d5b.jpg"
+  },
+  {
+    "revision": "1c5f72be6999fc53d6b9fb38de46c6f3",
+    "url": "/static/media/quiz.1c5f72be.png"
+  },
+  {
+    "revision": "9ac313913fca54dbdee81b8682877f98",
+    "url": "/static/media/rc2.9ac31391.jpg"
+  },
+  {
+    "revision": "d51d00738222093bd376fe8ea7f8dad7",
+    "url": "/static/media/saelogohd2.d51d0073.png"
+  },
+  {
+    "revision": "1b07e43698bd2101493d5c6c97605323",
+    "url": "/static/media/screws.1b07e436.png"
+  },
+  {
+    "revision": "16e498cb84ee0b13d80bba82efabfca6",
+    "url": "/static/media/self-driving.16e498cb.jpg"
+  },
+  {
+    "revision": "19cfa842b49cdd9cc7ebefb7b9ec407d",
+    "url": "/static/media/smartbike.19cfa842.jpg"
+  },
+  {
+    "revision": "f9a573dc6e099eb88081725b2da54440",
+    "url": "/static/media/sudeep.f9a573dc.jpg"
+  },
+  {
+    "revision": "fa8cb275a94ae0d594405d5baa69e71e",
+    "url": "/static/media/team.fa8cb275.png"
+  }
+]);
